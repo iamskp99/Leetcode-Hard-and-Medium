beb8083ceb98ab7d7cc2653aa13d27eafdff966b
Shashank Kumar Pandey <42648568+iamskp99@users.noreply.github.com>
2024-10-25 20:57:18 +0530

    Time: 171 ms (76.36%) | Memory: 22.1 MB (48.11%) - LeetSync

---
+++ 868-push-dominoes/push-dominoes.py
@@ -0,0 +1,72 @@
+class Solution:
+    def pushDominoes(self, dominoes: str) -> str:
+        visited = set()
+        n = len(dominoes)
+        ans = ['.' for i in range(n)]
+        prev = -1
+        stack = []
+        for i in range(n):
+            if dominoes[i] == 'L':
+                ans[i] = 'L'
+                if prev != -1:
+                    a = prev
+                    b = i
+                    a += 1
+                    b -= 1
+                    while a <= b:
+                        if a == b:
+                            visited.add(a)
+
+                        else:
+                            visited.add(a)
+                            visited.add(b)
+                            ans[a],ans[b] = 'R','L'
+
+                        a += 1
+                        b -= 1
+
+                else:
+                    while len(stack) > 0:
+                        ind = stack.pop()
+                        if ind in visited:
+                            stack = []
+                            break
+
+                        visited.add(ind)
+                        ans[ind] = 'L'
+
+                prev = -1
+
+            elif dominoes[i] == 'R':
+                ans[i] = 'R'
+                stack = []
+                prev = i
+
+            else:
+                stack.append(i)
+
+        i = n-1
+        stack = []
+        while i > -1:
+            x = dominoes[i]
+            if x == 'R':
+                while len(stack) > 0:
+                    ele = stack.pop()
+                    if ele in visited:
+                        break
+
+                    ans[ele] = 'R'
+
+            elif x == 'L':
+                stack = []
+
+            else:
+                stack.append(i)
+
+            i -= 1
+
+        return "".join(ans)
+
+                
+
+                        
