d7041f4c86471a18818dc7eaa4607ac677ae827b
Shashank Kumar Pandey <42648568+iamskp99@users.noreply.github.com>
2025-06-21 13:04:11 +0530

    Time: 1 ms (80.08%) | Memory: 18.6 MB (35.69%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -3,60 +3,36 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        if len(matrix) == 1:
-            gg = matrix[0].count(0)
-            if gg > 0:
-                for i in range(len(matrix[0])):
-                    matrix[0][i] = 0
-
-            return matrix
-
-        if len(matrix[0]) == 1:
-            gg = 0
-            for i in range(len(matrix)):
-                if matrix[i][0] == 0:
-                    gg += 1
-
-            if gg > 0:
-                for i in range(len(matrix)):
-                    matrix[i][0] = 0
-
-            return matrix
-
         n,m = len(matrix),len(matrix[0])
-        flag,gflag = 0,0
+        exception_flag = 0
         for i in range(n):
             for j in range(m):
-                if matrix[i][j] == 0:
+                if matrix[i][j] != 0:
+                    continue
+                else:
                     if i == 0:
-                        flag = 1
-
+                        exception_flag = 1
+                        matrix[0][j] = 0
                     else:
                         matrix[i][0] = 0
-
-                    if j == 0:
-                        gflag = 1
-
-                    else:
                         matrix[0][j] = 0
 
+        # print(matrix)
         for i in range(1,n):
-            if matrix[i][0] == 0:
+            flag = 1 if matrix[i][0] == 0 else 0
+            if flag:
                 for j in range(1,m):
                     matrix[i][j] = 0
 
-        for j in range(1,m):
-            if matrix[0][j] == 0:
-                for i in range(1,n):
-                    matrix[i][j] = 0
+        # print(matrix)
+        for i in range(m):
+            flag = 1 if matrix[0][i] == 0 else 0
+            if flag:
+                for j in range(1,n):
+                    matrix[j][i] = 0
 
-        if flag:
+        if exception_flag:
             for i in range(m):
                 matrix[0][i] = 0
 
-        if gflag:
-            for i in range(n):
-                matrix[i][0] = 0
-
-        return matrix
-
+        return
