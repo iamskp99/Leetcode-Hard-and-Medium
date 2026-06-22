86266b533af8e433ca99bf5f315dc2fe13ed56cd
Shashank Kumar Pandey <42648568+iamskp99@users.noreply.github.com>
2026-06-22 22:05:56 +0530

    Time: 5427 ms (10.13%) | Memory: 19.5 MB (26.54%) - LeetSync

--- 79-word-search/word-search.py
+++ 79-word-search/word-search.py
@@ -1,36 +1,43 @@
 class Solution:
-    def rec(self,x,y,i,mat,board,n,m,word):
-        if i == len(word):
-            return True
-
-        if x < 0 or y < 0 or x == n or y == m:
+    def rec(self,i,r,c):
+        if i == len(self.word)-1:
+            if self.board[r][c] == self.word[i]:
+                return True
             return False
 
-        if mat[x][y] == 0:
-            return False
+        neighbours = [(0,-1),(0,1),(-1,0),(1,0)]
+        self.visited.add((r,c))
+        cur_ans = False
+        for x in neighbours:
+            if r+x[0] < 0 or r+x[0] >= self.n:
+                continue
 
-        if board[x][y] != word[i]:
-            return False
+            if c+x[1] < 0 or c+x[1] >= self.m:
+                continue
 
-        ans = False
-        o = [(0,1),(0,-1),(1,0),(-1,0)]
-        mat[x][y] = 0
-        for ele in o:
-            ans = ans | self.rec(x+ele[0],y+ele[1],i+1,mat,board,n,m,word)
+            if (r+x[0],c+x[1]) in self.visited:
+                continue
 
-        mat[x][y] = 1
-        return ans
+            if self.board[r+x[0]][c+x[1]] == self.word[i+1]:
+                cur_ans = cur_ans|self.rec(i+1,r+x[0],c+x[1])
+
+        self.visited.remove((r,c))
+        return cur_ans
 
     def exist(self, board: List[List[str]], word: str) -> bool:
-        n,m = len(board),len(board[0])
-        mat = []
-        for i in range(n):
-            o = [1 for j in range(m)]
-            mat.append(o)
-        
+        self.board = board
+        self.word = word
+        self.visited = set()
+        n,m = len(self.board),len(self.board[0])
+        self.n,self.m = n,m
         for i in range(n):
             for j in range(m):
-                if self.rec(i,j,0,mat,board,n,m,word):
-                    return True
+                if self.board[i][j] == self.word[0]:
+                    ans = self.rec(0,i,j)
+                    if ans:
+                        return True
 
         return False
+
+
+# [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]]
